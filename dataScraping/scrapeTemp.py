9c3d72d9938e0c0a4fc099b033fedfe692aca183
eduardoortegathethird <eduardoortega.3@outlook.com>
2021-01-10 17:29:05 -0800

    made a TODO

--- dataScraping/scrapeTemp.py
+++ dataScraping/scrapeTemp.py
@@ -1,1 +1,7 @@
-TODO: create (e.g. google image etc.) scraping methods here
+#TODO: look into beautiful soup and requests libraries for queries information
+
+def main():
+	print("Connecting to server to scrape images.....")
+
+if __name__ == "__main__":
+	main()
