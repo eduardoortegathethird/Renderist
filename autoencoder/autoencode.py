c1050d909a660ed15e4186389929660a1d2d5168
eduardoortegathethird <eduardoortega.3@outlook.com>
2021-01-10 18:44:57 -0800

    pulled, batched, and shuffled MNIST dataset

--- autoencoder/autoencode.py
+++ autoencoder/autoencode.py
@@ -8,8 +8,54 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 import time
 
+'''
+Function: load_mnist_dataset
+Args: N/A
+Return: the MNIST dataset
+Description:
+'''
+def load_mnist_dataset():
+    return tf.keras.datasets.mnist.load_data()
+
+'''
+Function: preprocess_images
+Args: image_set
+Return:
+Description:
+'''
+def preprocess_images(image_set):
+    prepped_image_set = image_set.reshape((image_set.shape[0], 28, 28, 1)) / 255.
+    return np.where(prepped_image_set > 0.5, 1.0, 0.0).astype('float32')
+
+'''
+Function: batch_and_shuffle_data
+Args: image_set, set_size, batch_size
+Return: batched and shuffled dataset (from image dataset)
+Description:
+'''
+def batch_and_shuffle_data(image_set, set_size, batch_size):
+    return (tf.data.Dataset.from_tensor_slices(image_set).shuffle(set_size).batch(batch_size))
+
+'''
+Fuction: MAIN
+Args: N/A
+Return:
+Description: Utilizing MNIST datasets to feed into tensorflow based vcariational autoencoder
+'''
 def main():
-    print("Insert main methods here")
+    #loading the mnist dataset
+    (train_images, _), (test_images, _) = load_mnist_dataset()
+	
+    #prepping the image data sets, both training and testing
+    train_images = preprocess_images(train_images)
+    test_images = preprocess_images(test_images)
+    train_size, batch_size, test_size = 60000, 32, 10000
+    
+    #shuffling the datasets to ensure randomness
+    train_dataset = batch_and_shuffle_data(train_images, train_size, batch_size)
+    test_dataset = batch_and_shuffle_data(test_images, test_size, batch_size)
+
+    
 
 if __name__ == "__main__":
     main()
---
+++ autoencoder/convolutionalVariationalAutoencodeNetwork.py
@@ -0,0 +1,1 @@
+#TODO create network for the CVAE to run through the datasets
