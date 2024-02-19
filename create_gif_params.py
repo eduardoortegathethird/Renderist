5c1d160700bbf1016285d452fd0a92a5652a005f
eeortega <eeortega@en4226599rl.eng.asu.edu>
2024-02-19 05:16:54 -0700

    new

---
+++ create_gif_params.py
@@ -0,0 +1,17 @@
+# Author | Eduardo Ortega
+import matplotlib.pyplot as plt
+import numpy as np
+
+ax = plt.figure().add_subplot(projection='3d')
+
+# Prepare arrays x, y, z
+theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+z = np.linspace(-2, 2, 100)
+r = z**2 + 1
+x = r * np.sin(theta)
+y = r * np.cos(theta)
+
+ax.plot(x, y, z, label='parametric curve')
+ax.legend()
+plt.savefig('fig.png')
+plt.show()
