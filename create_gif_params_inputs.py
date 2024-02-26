d387c34c43bfba514ce7da4d827ffefc6f96042d
eeortega <eeortega@en4226599rl.eng.asu.edu>
2024-02-26 00:07:02 -0700

    new

--- create_gif_params_inputs.py
+++ create_gif_params_inputs.py
@@ -9,9 +9,9 @@ from PIL import Image
 
 
 def convert_parametric(x, y, z):
-    sw_l = [abs(s/4) for s in x]
-    cw_l = [abs(s/4) for s in y]
-    tvw_l = [2**s for s in z]
+    sw_l = [abs(s/5) for s in x]
+    cw_l = [abs(s/5) for s in y]
+    tvw_l = [6**s for s in z]
     return sw_l, cw_l, tvw_l
 
 # Prepare inputs
